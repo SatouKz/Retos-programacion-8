38060b483b07815a9fe52dc90f4a66691d8c8677
SatouKz <alexander.ajalla404@gmail.com>
2022-01-17 01:10:13 -0300

    Reto completado

--- main.js
+++ main.js
@@ -1,30 +1,37 @@
 const cosasQueAprendimos = [
   {
-    tema: 'terminal',
-    class: '',
+    tema: "terminal",
+    class: "",
   },
   {
-    tema: 'node',
-    class: '',
+    tema: "node",
+    class: "",
   },
   {
-    tema: 'oop',
-    class: '',
+    tema: "oop",
+    class: "",
   },
   {
-    tema: 'flexbox',
-    class: '',
+    tema: "flexbox",
+    class: "",
   },
   {
-    tema: 'css',
-    class: '',
+    tema: "css",
+    class: "",
   },
   {
-    tema: 'dom',
-    class: 'special',
+    tema: "dom",
+    class: "special",
   },
 ];
 
-function main() {}
+function main() {
+  cosasQueAprendimos.forEach((cosa) => {//forEach para recorrer el array
+    const nuevoItem = document.createElement("li"); //crear elemento li
+    nuevoItem.innerText = cosa.tema; //texto del li
+    cosa.class && nuevoItem.classList.add(cosa.class); //si el elemento existe agregar el nuevoItem
+    document.querySelector("ul.lista").appendChild(nuevoItem); // seleccion de la lista y agregar hijo
+  });
+}
 
 main();
